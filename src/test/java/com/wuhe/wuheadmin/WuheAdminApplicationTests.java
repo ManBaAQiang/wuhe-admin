e6cf469af776f17a876fc8fc259682d5ebd025e4
ManBaAQiang <1216617578@qq.com>
2020-08-25 16:04:41 +0800

    admin 初始提交 Test

---
+++ src/test/java/com/wuhe/wuheadmin/WuheAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuhe.wuheadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuheAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
